1e3062c7e447175cd20c5322f6871034ac09be44
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-07-12 23:48:13 -0700

    Updates

---
+++ precache-manifest.7830455dd0b78f3fb3fdec1393a6edbb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d68311b7cc635895b4ed1e6d070e59e",
+    "url": "/Reactjs-Portfolio/index.html"
+  },
+  {
+    "revision": "1c2cb2977d6e1a1aa6e5",
+    "url": "/Reactjs-Portfolio/static/css/main.451627d9.chunk.css"
+  },
+  {
+    "revision": "6ccb9917b6370506be36",
+    "url": "/Reactjs-Portfolio/static/js/2.a1bd3bd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reactjs-Portfolio/static/js/2.a1bd3bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c2cb2977d6e1a1aa6e5",
+    "url": "/Reactjs-Portfolio/static/js/main.1baf4315.chunk.js"
+  },
+  {
+    "revision": "8c4a957da461c29f2ee4",
+    "url": "/Reactjs-Portfolio/static/js/runtime-main.67a54cc9.js"
+  },
+  {
+    "revision": "ab8561a59b3a305a3c30663220c75f3c",
+    "url": "/Reactjs-Portfolio/static/media/BT2.ab8561a5.PNG"
+  },
+  {
+    "revision": "07770c402707472c2e53dff92f835807",
+    "url": "/Reactjs-Portfolio/static/media/BudgetTracker1.07770c40.PNG"
+  },
+  {
+    "revision": "084154d96359b051c29c6d76acd31386",
+    "url": "/Reactjs-Portfolio/static/media/Employee-Directory-1.084154d9.PNG"
+  },
+  {
+    "revision": "ff8e87b76b10c3713c0218341e698779",
+    "url": "/Reactjs-Portfolio/static/media/Note-taker.ff8e87b7.PNG"
+  },
+  {
+    "revision": "c827f8c8e82ecfe4bb992c8ddb34c6f7",
+    "url": "/Reactjs-Portfolio/static/media/Pass-gen.c827f8c8.PNG"
+  },
+  {
+    "revision": "ac843943f7b9501caf113d3fa4a7a8e5",
+    "url": "/Reactjs-Portfolio/static/media/THF3.ac843943.PNG"
+  },
+  {
+    "revision": "1a345f3832aed955a359ec0461094151",
+    "url": "/Reactjs-Portfolio/static/media/gabriele-motter-YOwu-KFBRmU-unsplash.1a345f38.jpg"
+  }
+]);
